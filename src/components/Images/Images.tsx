5d1fd88594d34315d2d4724656dbb535cd3beeea
bluewood <bluewood.truth@gmail.com>
2021-04-24 22:35:51 +0900

    feat: implement "Images" component

--- src/components/Images/Images.tsx
+++ src/components/Images/Images.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import ImageItem from "components/Images/ImageItem";
+import { IImage } from "types/types";
 
-const Images = () => {
-    return <div>images</div>;
+interface IImagesProps {
+  imageDatas: IImage[];
+}
+
+const Images = (props: IImagesProps) => {
+  return (
+    <div>
+      {props.imageDatas.map((imageData: IImage) => {
+        return <ImageItem imageData={imageData} />;
+      })}
+    </div>
+  );
 };
 
 export default Images;
