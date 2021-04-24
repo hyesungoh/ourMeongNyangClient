f8fd15860fb55eb43cf1b2420a80b785153ed6a9
hyesungoh <haesungoh414@gmail.com>
2021-04-24 19:37:58 +0900

    Install pakages

---
+++ src/container/Container.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Input from "components/Input";
+import Images from "components/Images";
+
+const Container = () => {
+    return (
+        <main>
+            <Input />
+            <Images />
+        </main>
+    );
+};
+
+export default Container;
