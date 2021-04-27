e73610ca8084fbc28c1543801401fbc17ccc6104
bluewood <bluewood.truth@gmail.com>
2021-04-27 15:40:39 +0900

    feat: remove "Loader" and "EndMessage" components

--- src/components/Images/Scroll.tsx
+++ src/components/Images/Scroll.tsx
@@ -1,11 +1,7 @@
 export const Loader = () => {
-  return <p>Loading...</p>;
+  return null;
 };
 
 export const EndMessage = () => {
-  return (
-    <p style={{ textAlign: "center" }}>
-      <b>Yay! You have seen it all</b>
-    </p>
-  );
+  return null;
 };
