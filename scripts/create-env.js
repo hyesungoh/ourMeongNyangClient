8adca691777299d1202c68cb6f336133596a9528
hyesungoh <haesungoh414@gmail.com>
2021-04-28 02:46:03 +0900

    fix: fix create-env

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,22 +1,13 @@
 const fs = require("fs");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 dotenv.config();
 
-fs.writeFileSync("./.env", `REACT_APP_APIKEY=${process.env.REACT_APP_APIKEY}`);
 fs.writeFileSync(
     "./.env",
-    `REACT_APP_AUTHDOMAIN=${process.env.REACT_APP_AUTHDOMAIN}`
+    `REACT_APP_APIKEY=${process.env.REACT_APP_APIKEY}
+REACT_APP_AUTHDOMAIN=${process.env.REACT_APP_AUTHDOMAIN}
+REACT_APP_PROJECTID=${process.env.REACT_APP_PROJECTID}
+REACT_APP_STORAGEBUCKET=${process.env.REACT_APP_STORAGEBUCKET}
+REACT_APP_MESSAGINGSENDERID=${process.env.REACT_APP_MESSAGINGSENDERID}
+REACT_APP_APPID=${process.env.REACT_APP_APPID}`
 );
-fs.writeFileSync(
-    "./.env",
-    `REACT_APP_PROJECTID=${process.env.REACT_APP_PROJECTID}`
-);
-fs.writeFileSync(
-    "./.env",
-    `REACT_APP_STORAGEBUCKET=${process.env.REACT_APP_STORAGEBUCKET}`
-);
-fs.writeFileSync(
-    "./.env",
-    `REACT_APP_MESSAGINGSENDERID=${process.env.REACT_APP_MESSAGINGSENDERID}`
-);
-fs.writeFileSync("./.env", `REACT_APP_APPID=${process.env.REACT_APP_APPID}`);
